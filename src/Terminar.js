b347f8a49bf9d115b0f52b3efe08f2b557b445d0
rnav413 <69471412+rnav413@users.noreply.github.com>
2023-03-08 16:42:19 -0500

    Update Terminar.js

--- src/Terminar.js
+++ src/Terminar.js
@@ -1,8 +1,7 @@
 const Terminar = (listaChances) => {
     let totalDinero = 0;
     listaChances.forEach((chance) => (totalDinero += parseInt(chance.dinero)));
-    alert(`Chances: ${JSON.stringify(listaChances)} \nTotal Dinero: ${totalDinero}`);
-   
+    return totalDinero
 }
 
 
