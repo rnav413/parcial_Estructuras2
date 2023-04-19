1518444a45f9f3d3737c570731c9e0c59c073bed
rnav413 <69471412+rnav413@users.noreply.github.com>
2023-04-19 16:07:04 -0500

    Delete Terminar.js

--- src/Terminar.js
+++
@@ -1,9 +0,0 @@
-const Terminar = (listaChances) => {
-    let totalDinero = 0;
-    listaChances.forEach((chance) => (totalDinero += parseInt(chance.dinero)));
-    alert(`Chances: ${JSON.stringify(listaChances)} \nTotal Dinero: ${totalDinero}`);
-   
-}
-
-
-export default Terminar
